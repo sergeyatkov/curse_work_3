0a195ee6378335be8f86e5e74c1822eee6de8fd7
Sergey Atkov <bersercwow@gmail.com>
2023-12-15 17:13:12 +0700

    add a function for encrypting transaction account details

--- utils/operation.py
+++ utils/operation.py
@@ -19,3 +19,21 @@ class Operation:
         self.from_ = from_
         self.to = to
 
+    def encrypts_details(self, details: str) -> str:
+        """
+        Функция приводит номера реквизитов для переводов
+        в заданный зашифрованный формат
+        :param details: строка с данными счета
+        :return: зашифрованный номер счета
+        """
+        prop_number_list = []
+        for symbol in details:
+            if symbol.isdigit():
+                prop_number_list.append(symbol)
+        prop_number_str = "".join(prop_number_list)
+        if len(prop_number_str) > 16:
+            return f"****************{prop_number_str[16:]}"
+        elif len(prop_number_str) == 16:
+            return f"{prop_number_str[0: 4]} {prop_number_str[4: 6]}** **** {prop_number_str[12:]}"
+        else:
+            return prop_number_str
