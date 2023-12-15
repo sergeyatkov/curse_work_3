788966ad96d1b45a99bdb105e186ca7682d6bf98
Sergey Atkov <bersercwow@gmail.com>
2023-12-15 14:50:25 +0700

    add class Operation

--- utils/operation.py
+++ utils/operation.py
@@ -1,2 +1,21 @@
 class Operation:
-    pass
+    """
+    Класс для обработки данных по операциям
+    """
+    def __init__(self,
+                 tn: int,
+                 date: str,
+                 state: str,
+                 operation_amount: dict,
+                 description: str,
+                 from_: str,
+                 to: str
+                 ):
+        self.tn = tn
+        self.date = date
+        self.state = state
+        self.operation_amount = operation_amount
+        self.description = description
+        self.from_ = from_
+        self.to = to
+
