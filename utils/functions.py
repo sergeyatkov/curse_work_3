59d8fe1c57570cea306d1c0269acb4326b44d908
Sergey Atkov <bersercwow@gmail.com>
2023-12-15 15:43:09 +0700

    Revert "Revert "add a function for finding source data in each transaction""
    This reverts commit 1a3f19104ad1dae22619180c6d6d3140f10ad951.

--- utils/functions.py
+++ utils/functions.py
@@ -1,7 +1,9 @@
 import json
 
+from utils.operation import Operation
 
-def get_all_transaction_data(path):
+
+def get_all_transaction_data(path) -> list[dict]:
     """
     Функция берет данные из файла json
     :param path: путь к файлу
@@ -9,3 +11,24 @@ def get_all_transaction_data(path):
     """
     with open(path, encoding="utf-8") as file:
         return json.load(file)
+
+
+def get_individual_transactions(transactions: list[dict]) -> list[dict]:
+    """
+    Функция приводит данные по операциям к нужному виду
+    :param transactions: все данные по операциям
+    :return: список с неодходимыми данными по каждой операции
+    """
+    correct_transactions = []
+    for transaction in transactions:
+            existing_transaction = Operation(
+                tn=transaction["id"],
+                date=transaction["date"],
+                state=transaction["state"],
+                operation_amount=transaction["operationAmount"],
+                description=transaction["description"],
+                from_=transaction["from"],
+                to=transaction["to"]
+            )
+        correct_transactions.append(existing_transaction)
+    return correct_transactions
